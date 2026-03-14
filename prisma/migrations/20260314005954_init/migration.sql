045fe96812baf974a31356c8283a3046914b5611
Dev-Jhoncel <jhoncel.cadiena@mlhuillier.com>
2026-03-14 15:32:30 +0800

    chore:[Jhoncel]Add prisma in the project

---
+++ prisma/migrations/20260314005954_init/migration.sql
@@ -0,0 +1,2 @@
+-- AlterTable
+ALTER TABLE `links` ADD COLUMN `deletedAt` DATETIME(3) NULL;
--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,9 @@
-import { Injectable } from '@nestjs/common';
+import { Get, Injectable } from '@nestjs/common';
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+  @Get(':shortCode')
+  getlink(shortCode: string): string {
+    return shortCode;
   }
 }
